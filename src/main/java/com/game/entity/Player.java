b42abe1673a805edd4abe8dd0923b9b2273d027a
Sergei Sokolov <sokol16.90@mail.ru>
2023-01-19 22:53:44 +0300

    done

---
+++ src/main/java/com/game/entity/Player.java
@@ -0,0 +1,147 @@
+package com.game.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "player")
+public class Player {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "race")
+    @Enumerated(EnumType.STRING)
+    private Race race;
+    @Column(name = "profession")
+    @Enumerated(EnumType.STRING)
+    private Profession profession;
+    @Column(name = "birthday")
+    private Date birthday;
+    @Column(name = "banned")
+    private Boolean banned;
+    @Column(name = "experience")
+    private Integer experience;
+    @Column(name = "level")
+    private Integer level;
+    @Column(name = "untilNextLevel")
+    private Integer untilNextLevel;
+
+    // TODO: 19.01.2023  В проекте должна использоваться сущность Player, которая имеет поля
+//Long id ID игрока
+//String name Имя персонажа (до 12 знаков включительно)
+//String title Титул персонажа (до 30 знаков включительно)
+//Race race Расса персонажа
+//Profession profession Профессия персонажа
+//Integer experience Опыт персонажа. Диапазон значений 0..10,000,000
+//Integer level Уровень персонажа
+//Integer untilNextLevel Остаток опыта до следующего уровня
+//Date birthday Дата регистрации
+//Диапазон значений года 2000..3000 включительно
+//Boolean banned Забанен / не забанен:
+
+
+    public Player() {
+    }
+
+    public Player(Long id, String name, String title, Race race, Profession profession, Integer experience,
+                  Integer level, Integer untilNextLevel, Date birthday, Boolean banned) {
+        this.id = id;
+        this.name = name;
+        this.title = title;
+        this.race = race;
+        this.profession = profession;
+        this.experience = experience;
+        this.level = level;
+        this.untilNextLevel = untilNextLevel;
+        this.birthday = birthday;
+        this.banned = banned;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Race getRace() {
+        return race;
+    }
+
+    public void setRace(Race race) {
+        this.race = race;
+    }
+
+    public Profession getProfession() {
+        return profession;
+    }
+
+    public void setProfession(Profession profession) {
+        this.profession = profession;
+    }
+
+    public Integer getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Integer experience) {
+        this.experience = experience;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Integer getUntilNextLevel() {
+        return untilNextLevel;
+    }
+
+    public void setUntilNextLevel(Integer untilNextLevel) {
+        this.untilNextLevel = untilNextLevel;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Boolean getBanned() {
+        return banned;
+    }
+
+    public void setBanned(Boolean banned) {
+        this.banned = banned;
+    }
+    public Boolean isBanned() {
+        return banned;
+    }
+}
